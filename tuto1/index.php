3ba8fb4f298c68cc359c3a718c7676d1e301f829
Ignatiusdork <littignatius@gmail.com>
2024-01-26 18:26:08 +0100

    laracast-own

---
+++ tuto1/index.php
@@ -0,0 +1,60 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<?php 
+
+$books = [
+    [
+        'name' => '12 Rules',
+        'author' => 'Jordan Peterson',
+        'releaseYear' => 2018,
+        'genre' => 'Philosophy',
+        'purchaseUrl' => 'http://example.com'
+    ],
+
+    [
+        'name' => 'Art Of War',
+        'author' => 'Sun Tzu',
+        'releaseYear' => 2019,
+        'genre' => 'Philosophy/Phychology',
+        'purchaseUrl' => 'http://example.com'
+    ],
+
+    [
+        'name' => 'Art Of War',
+        'author' => 'Sun Tzu',
+        'releaseYear' => 2019,
+        'genre' => 'Philosophy/Phychology',
+        'purchaseUrl' => 'http://example.com'
+    ],
+
+    [
+        'name' => 'Blink',
+        'author' => 'Malcolm Gladwell',
+        'releaseYear' => 1968,
+        'genre' => 'Philosophy/Phychology',
+        'purchaseUrl' => 'http://example.com'
+    ],
+];
+
+function filter($items, $fn) {
+    $filteredItems = [];
+
+    foreach ($items as $item) {
+        if ($fn($item)) {
+            $filteredItems[] = $item;
+        }
+    }
+    return $filteredItems;
+};
+
+$filteredBooks = array_filter($books,function ($book) {
+    return $book["releaseYear"] >= 1950 && $book['releaseYear'] <= 2020;
+});
+
+require 'index.view.php';
+
+
