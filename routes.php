0f5b3b7670dfcddc1e38384ec3fdf35f5a76b174
ignatiusdork <littignatius@gmail.com>
2024-02-12 18:16:52 +0100

    Router-system-works

--- routes.php
+++ routes.php
@@ -1,15 +1,11 @@
 <?php
 
-// return [
-//     '/laracasts/' => 'controllers/index.php',
-//     '/laracasts/index' => 'controllers/index.php',
-//     '/laracasts/about' => 'controllers/about.php',
-//     '/laracasts/notes' => 'controllers/notes/index.php',
-//     '/laracasts/note' => 'controllers/notes/show.php',
-//     '/laracasts/notes/create' => 'controllers/notes/create.php',
-//     '/laracasts/contact' => 'controllers/contact.php',
-// ];
-
+$router->get('/laracasts/', 'controllers/index.php');
 $router->get('/laracasts/index', 'controllers/index.php');
-$router->delete('/laracasts/note', 'controllers/notes/destory.php');
+$router->get('/laracasts/about', 'controllers/about.php');
+$router->get('/laracasts/contact', 'controllers/contact.php');
+
+$router->get('/laracasts/notes', 'controllers/notes/index.php');
+$router->get('/laracasts/note', 'controllers/notes/show.php');
+$router->get('/laracasts/notes/create', 'controllers/notes/create.php');
 
