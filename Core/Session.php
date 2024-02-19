6366a0cf77474a7179a4065802eed66c7d2511a3
ignatiusdork <littignatius@gmail.com>
2024-02-19 16:48:22 +0100

    sessions-repost form data

--- Core/Session.php
+++ Core/Session.php
@@ -26,4 +26,17 @@ class Session {
     public static function unflash() {
         unset($_SESSION['_flash']);
     }
+
+    public static function flush() {
+        $_SESSION = [];
+    }
+
+    public static function destory() {
+        static::flush();
+
+        session_destroy();
+    
+        $params = session_get_cookie_params();
+        setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']); 
+    }
 }
--- Http/controllers/session/create.php
+++ Http/controllers/session/create.php
@@ -1,7 +1,5 @@
 <?php
 
-//$errors = isset($_SESSION['_flash']['errors']) ? $_SESSION['_flash']['errors'] : [];
-
 view('session/create.view.php', [
     'errors' => $_SESSION['_flash']['errors'] ?? []
 ]);
